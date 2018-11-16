4a4b8f52ba062d9e9252558ac1e2d8dc7bac613b
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2018-11-16 23:09:01 +0300

    2018-11-16 Сделал структуру

---
+++ blocks/aside.php
@@ -0,0 +1,19 @@
+<aside class="col-md-4">
+    <div class="p-3 mb-3 bg-warning rounded">
+        <h4>
+            <b>
+                Тестовый блок
+            </b>
+        </h4>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem fuga iusto laborum, omnis voluptates nisi sapiente aperiam reprehenderit voluptate!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem fuga iusto laborum, omnis voluptates nisi sapiente aperiam reprehenderit voluptate!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem fuga iusto laborum, omnis voluptates nisi sapiente aperiam reprehenderit voluptate!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem fuga iusto laborum, omnis voluptates nisi sapiente aperiam reprehenderit voluptate!
+        </p>
+    </div>
+</aside>
--- index.php
+++ index.php
@@ -1,13 +1,28 @@
 <!doctype html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>NEPOGODA.BY</title>
+
+    <?php
+    $website_title = 'NEPOGODA.BY';
+    require 'blocks/head.php';
+    ?>
+
 </head>
 <body>
+    <?php require 'blocks/header.php'; ?>
 
+    <main class="container mt-5">
+        <div class="row">
+            <div class="col-md-8 mb-3">
+
+
+
+
+            </div>
+            <?php require 'blocks/aside.php' ?>
+        </div>
+    </main>
+
+    <?php require 'blocks/footer.php' ?>
 </body>
 </html>
