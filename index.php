26b285bfd9d44c6a6b0d0dd4d0c892d847e194c4
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2018-11-16 21:53:21 +0300

    2018-11-16 Изменил индекс

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 16.11.2018
- * Time: 21:50
- */
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>NEPOGODA.BY</title>
+</head>
+<body>
+
+</body>
+</html>
