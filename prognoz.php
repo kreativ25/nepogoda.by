7e42450906721be8f9b93fe1b51d4735a9511d99
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2018-12-05 23:34:13 +0300

    2018-12-05 Сделал передачу годода в API, Определеил переменные API. Установил таймзону.

--- prognoz.php
+++ prognoz.php
@@ -17,6 +17,11 @@
 
             <?php require_once 'functions.php';?>
             <?php
+
+            define("API_KEY", "ab06caeacd9e4f38c2b216a394d0ff11");
+            date_default_timezone_set('Europe/Minsk');   //Устанавливаем таймзону Беларуси
+
+
             $gorod_poisk = trim(filter_var($_POST['gorod_poisk'],FILTER_SANITIZE_STRING));
             $error = '';
 
@@ -40,7 +45,35 @@
             $query->execute([$gorod_poisk]);
             $gorod_id = $query->fetchColumn();
 
-            echo $gorod_id;
+            $link = 'http://api.openweathermap.org/data/2.5/forecast?id=' . $gorod_id . '&appid=' . API_KEY . '&units=metric';
+            $api = file_get_contents($link);
+            $nepogoda = json_decode($api, true);
+            $nepogoda_count = count($nepogoda['list']); //количество элементов в массиве API для цикла
+
+
+
+            //Определяем переменные для пополнения БД
+            $data_prognoza = $nepogoda["list"][$i]["dt_txt"]; //ОБРАБОТАТЬ - получить только дату
+            $time_prognoza = $nepogoda["list"][$i]["dt_txt"]; //ОБРАБОТАТЬ - получить только время
+            $date_bd = date("Y-m-d");
+            $time_bd = date("H:i:s");
+            $city_id = $nepogoda["city"]["id"];
+            $temp = $nepogoda["list"][$i]["main"]["temp"];
+            $temp_max = $nepogoda["list"][$i]["main"]["temp_max"];
+            $temp_min = $nepogoda["list"][$i]["main"]["temp_min"];
+            $skorost_vetra = $nepogoda["list"][$i]["wind"]["speed"];
+            $napravleniye_vetra = $nepogoda["list"][$i]["wind"]["deg"];
+            $atmosfernoye_davleniye = $nepogoda["list"][$i]["main"]["sea_level"];
+            $vlazhnost = $nepogoda["list"][$i]["main"]["humidity"];
+            $usloviya = $nepogoda["list"][$i]["weather"][0]["description"];
+
+
+            for($i = 0; $i < $nepogoda_count; $i++){
+                if($nepogoda["list"][$i]["dt_txt"] != ""){
+                    echo $nepogoda["list"][$i]["dt_txt"] . " = " . $nepogoda["list"][$i]["weather"][0]["description"];
+                    echo "<br>";
+                }
+            };
 
             ?>
 
