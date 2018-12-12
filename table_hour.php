73033a4425e149ad6b786b5d80535cdb01a7b5cf
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2018-12-12 23:47:25 +0300

    2018-12-12 Делаю правильную таблицу

--- table_hour.php
+++ table_hour.php
@@ -55,6 +55,10 @@
             echo $gorod_id;
             echo "</br>";
 
+
+
+
+
             //*********************************************************
 
             echo "</br>";
@@ -86,168 +90,22 @@
             echo "</br>";
             echo "</br>";
 
-          //---------------------------------------------РРРРРРРРРРРРРРРРРРРРРРРРРРРРРРРР------------------------------------
             //Начало новой таблицы
             echo '
-                <div class="d-flex bd-highlight">
-                      <div class="p-2 flex-fill bd-highlight bg-dark text-white">Дата</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-dark text-white">t°C</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-dark text-white">Flex item</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-dark text-white">Flex item</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-dark text-white">Ветер, м/с</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-dark text-white">Влажность, %</div>
-                </div>
-                ';
-
-
-
-
-            for ($i = 0; $i < $gorod_id_COUNT; $i++){
-
-                //добавляем плюс к положительным цифрам, убираем отрицательный ноль и тд.
-                if ($gorod_id[$i]["temp"] > 0){
-                    $gorod_id_print =  "+".$gorod_id[$i]["temp"];
-                };
-
-                if ($gorod_id[$i]["temp"] < 0){
-                    $gorod_id_print =  $gorod_id[$i]["temp"];
-                };
-
-                if ($gorod_id[$i]["temp"] == "-0"){
-                    $gorod_id_print =  0;
-                };
-
-
-                //убираем дублирующиеся записи при обновлении БД
-                $x = $i - 1;
-                if ($gorod_id[$i]["data_prognoza"] == $gorod_id[$x]["data_prognoza"] and
-                    $gorod_id[$i]["time_prognoza"] == $gorod_id[$x]["time_prognoza"]){
-                    continue;
-                };
-
-                //выводим название дней недели на русском
-                $den_nedeli = strftime("%w", strtotime($gorod_id[$i]["data_prognoza"]));
-
-                //вытягиваем день месяца
-                $date = date_create($gorod_id[$i]["data_prognoza"]);
-                $month_day = date_format($date, 'd');
-
-                $den_nedeli_rus = "";
-                switch ($den_nedeli){
-                    case 0:
-                        $den_nedeli_rus = 'Воскресенье';
-                        break;
-                    case 1:
-                        $den_nedeli_rus = 'Понедельник';
-                        break;
-                    case 2:
-                        $den_nedeli_rus = 'Вторник';
-                        break;
-                    case 3:
-                        $den_nedeli_rus = 'Среда';
-                        break;
-                    case 4:
-                        $den_nedeli_rus = 'Четверг';
-                        break;
-                    case 5:
-                        $den_nedeli_rus = 'Пятница';
-                        break;
-                    case 6:
-                        $den_nedeli_rus = 'Суббота';
-                        break;
-                };
-
-                //вытягиваем месяц на русском
-                $month = date_format($date, 'n');
-                switch ($month){
-                    case 1:
-                        $month_rus = 'января';
-                        break;
-                    case 2:
-                        $month_rus = 'февраля';
-                        break;
-                    case 3:
-                        $month_rus = 'марта';
-                        break;
-                    case 4:
-                        $month_rus = 'апреля';
-                        break;
-                    case 5:
-                        $month_rus = 'мая';
-                        break;
-                    case 6:
-                        $month_rus = 'июня';
-                        break;
-                    case 7:
-                        $month_rus = 'июля';
-                        break;
-                    case 8:
-                        $month_rus = 'августа';
-                        break;
-                    case 9:
-                        $month_rus = 'сентября';
-                        break;
-                    case 10:
-                        $month_rus = 'октября';
-                        break;
-                    case 11:
-                        $month_rus = 'ноября';
-                        break;
-                    case 12:
-                        $month_rus = 'декабря';
-                        break;
-                };
-
-                //делаем короткое время
-                $time_BD = date_create($gorod_id[$i]["time_prognoza"]);
-                $time_hort = date_format($time_BD, 'H:i');
-
-                //разделяем дни
-                if ($gorod_id[$i]["time_prognoza"] == '00:00:00'){
-                    echo '  
-                 <div class="d-flex bd-highlight">
-                      <div class="p-2 flex-fill bd-highlight bg-secondary text-white">' . $den_nedeli_rus .", " . $month_day . " " . $month_rus . '</div>
-                </div>
-                       
-                    ';
-                }
-                echo '
-                <div class="d-flex bd-highlight border-top">
-                      <div class="p-2 flex-fill bd-highlight bg-light text-dark">' . $time_hort . '</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-light text-dark">' . $gorod_id_print . '</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-light text-dark">' . '<img src="img/облачно_с_прояснениями.png" alt="небольшой дождь">' . '</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-light text-dark">' . $gorod_id[$i]["usloviya"] . '</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-light text-dark">' . $gorod_id[$i]["skorost_vetra"] . '</div>
-                      <div class="p-2 flex-fill bd-highlight  bg-light text-dark">' . $gorod_id[$i]["vlazhnost"] . '</div>
-                </div>
-                ';
-            }
-
-
-
-
-            echo "</br>";
-            echo "</br>";
-            echo "</br>";
-            echo "</br>";
-            //----------------------------------------------НННННННННННННННННННННННННННННН-------------------------------------
-
-            //заголовок таблицы
-            echo '
-            <table class="table table-sm">
-                <thead class="thead-light">
-                <tr>
-                    <th scope="col">Дата</th>
-                    <th scope="col">t°C</th>
-                    <th scope="col"></th>
-                    <th scope="col"></th>
-                    <th scope="col">Ветер, м/с</th>
-                    <th scope="col">Влажность, %</th>
-                </tr>
-                </thead>
-                <tbody>
+            <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                    <thead>
+                    <tr>
+                        <th>Дата</th>
+                        <th>t°с</th>
+                        <th>Условия</th>
+                        <th>Ветер, м/с</th>
+                        <th>Влажность, %</th>
+                    </tr>
+                    </thead>
+                    <tbody>
             ';
-            //вставляем строки
+
             for ($i = 0; $i < $gorod_id_COUNT; $i++){
 
                 //добавляем плюс к положительным цифрам, убираем отрицательный ноль и тд.
@@ -351,98 +209,30 @@
                 //разделяем дни
                 if ($gorod_id[$i]["time_prognoza"] == '00:00:00'){
                     echo '
-                    <thead class="thead-light">
-                        <tr >
-                            <th scope="col">' . $den_nedeli_rus .", " . $month_day . " " . $month_rus . '</th>
-                            <th scope="col"></th>
-                            <th scope="col"></th>
-                            <th scope="col"></th>
-                            <th scope="col"></th>
-                            <th scope="col"></th>
-                        </tr>
-                     </thead>
+                    
+                    <tr class="bg-secondary text-white">
+                        <td class="bg-secondary text-white"> ' . $den_nedeli_rus .", " . $month_day . " " . $month_rus . ' </td>
+                    </tr>
+                    
                     ';
                 }
-
-
                 echo '
-                 <tr>
-                    <td>' . $time_hort . '</td>
-                    <td>' . $gorod_id_print . '</td>
-                    <td>' . '<img src="img/облачно_с_прояснениями.png" alt="небольшой дождь">' . '</td>
-                    <td>' . $gorod_id[$i]["usloviya"] . '</td>
-                    <td>' . $gorod_id[$i]["skorost_vetra"] . '</td>
-                    <td>' . $gorod_id[$i]["vlazhnost"] . '</td>
-                </tr>
+                    <tr>
+                        <td>' . $time_hort . '</td>
+                        <td>' . $gorod_id_print . '</td>
+                        <td>' . '<img src="img/облачно_с_прояснениями.png" alt="небольшой дождь">' . " " . $gorod_id[$i]["usloviya"] . '</td>
+                        <td class="text-center">' . $gorod_id[$i]["skorost_vetra"] . '</td>
+                        <td class="text-center">' . $gorod_id[$i]["vlazhnost"] . '</td>
+                    </tr>
                 ';
             }
-            //закрытие тега создающего таблицу
+
             echo '
-                    </tbody>
-                    </table>';
-
-            //--------КОНЕЦ ТАБЛИЦЫ С ПРОГНОЗОМ-------
+             </tbody>
+             </table>
+             </div>
+            ';
             ?>
-
-
-            <?php
-            echo "</br>";
-            echo "</br>";
-            echo "</br>";
-            echo "</br>";
-            echo "</br>";
-            echo "</br>";
-            ?>
-
-
-
-
-
-
-            <table class="table table-sm">
-                <thead>
-                <tr>
-                    <th scope="col">Дата</th>
-                    <th scope="col">Температура</th>
-                    <th scope="col"></th>
-                    <th scope="col"></th>
-                    <th scope="col">Ветер, м/с</th>
-                    <th scope="col">Влажность, %</th>
-                </tr>
-                </thead>
-
-                <tbody>
-                <tr>
-                    <td>Mark</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td>Mark</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td>Mark</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-
-                </tbody>
-            </table>
-
-
-
-
 
         </div>
         <?php require 'blocks/aside.php' ?>
