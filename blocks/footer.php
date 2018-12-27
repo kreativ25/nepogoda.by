5322f287f43b20e9b15b0e34d431da0f46f7f3d9
kreativ_25 <aleksandr.a.kazakevich@gmail.com>
2018-12-27 16:57:33 +0300

    2018-12-27 Сделал список. Пока без разделения на блоки_v2

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer p-3 mb-2 bg-secondary" style="height: 60px">
     <div class="container">
-        <span class="text-white">Только правда о белорусской погоде...</span>
+        <span class="text-white">Только правда о белорусской непогоде...</span>
     </div>
 </footer>
--- spisok.php
+++ spisok.php
@@ -25,13 +25,6 @@
             </form>
 
             <?php
-                $city = 'Минск';
-
-                echo ' <a href="spisok_prognoz.php?gorod_poisk=' . $city . ' ">' . $city . '</a> ';
-
-             ?>
-
-            <?php
             //создаем массив с алфавитом
             mb_internal_encoding("UTF-8"); //устанавливаем кодировку для php
             $alphabet = array(
@@ -49,10 +42,6 @@
             //Создаем массив со списком городов
             $gorod_rus = $pdo->query('SELECT gorod_name_rus FROM gorod ORDER BY gorod_name_rus ASC')->fetchAll(PDO::FETCH_COLUMN);
 
-            //вытягиваем первую букву города
-            //mb_internal_encoding("UTF-8");
-            //$name_city_one = mb_substr($gorod_rus[0],0,1);
-
             //Выводим список городов
             echo '
                 <section class="banner-img py-5">
@@ -60,10 +49,10 @@
             ';
 
             for ($i = 0; $i < count($alphabet); $i++){
-                    //проверяем есть ли города подходящие для алфавита
-                    for ($x = 0; $x < $gorod_count; $x++){
-                        if ($alphabet[$i] == mb_substr($gorod_rus[$x],0,1)){
-                            echo '
+                //проверяем есть ли города подходящие для алфавита
+                for ($x = 0; $x < $gorod_count; $x++){
+                    if ($alphabet[$i] == mb_substr($gorod_rus[$x],0,1)){
+                        echo '
                             <div class="row text-center">
                                 <div class="col-md-12">
                                     <h2 class="text-left">' . $alphabet[$i] . '</h2>
@@ -71,25 +60,24 @@
                             </div> 
                             <div class="row">
                             <div class="col-md-4">
-                            ';
-                            break;
-                        }else{
-                            //continue;
-                        }
-                    }
-                    //добавляем название города в виде ссылки
-                    for ($a = 0; $a < $gorod_count; $a++){
-                        if ($alphabet[$i] == mb_substr($gorod_rus[$a],0,1)){
-                            echo '
                             <ul class="list-unstyled">
-                                <a href= "spisok_prognoz.php?gorod_poisk=' . $gorod_rus[$a] . ' "><li>' . $gorod_rus[$a] . '</li></a>
-                            </ul>
                             ';
-                        }
+                        break;
                     }
+                }
+                //добавляем название города в виде ссылки
+                for ($a = 0; $a < $gorod_count; $a++){
+                    if ($alphabet[$i] == mb_substr($gorod_rus[$a],0,1)){
+                        echo '
+                                <a href= "spisok_prognoz.php?gorod_poisk=' . $gorod_rus[$a] . ' "><li>' . $gorod_rus[$a] . '</li></a>
+                            ';
+                    }
+                }
                 echo '
+                    </ul>
                     </div>
                     </div>
+                    <br>
                  ';
             }
             echo '
